53fb7eeea9813478f627ff23f5a6e14a12e40920
Robbe Motmans <robbemotmans@hotmail.com>
2020-07-29 20:59:48 +0200

    rewrite test cases and testing

--- test/flightSurety.js
+++ test/flightSurety.js
@@ -1,14 +1,13 @@
-const Test = require('../config/testConfig.js');
-const BigNumber = require('bignumber.js');
+const test = require('../config/testConfig.js');
+const bigNumber = require('bignumber.js');
 const truffleAssert = require('truffle-assertions');
 
 contract('Flight Surety Tests', async (accounts) => {
 
   const timestamp = Math.floor(Date.now() / 1000);
-
   var config;
   before('setup contract', async () => {
-    config = await Test.Config(accounts);
+    config = await test.Config(accounts);
     await config.flightSuretyData.authorizeCaller(config.flightSuretyApp.address);
   });
 
@@ -93,11 +92,11 @@ contract('Flight Surety Tests', async (accounts) => {
     assert.equal(airline[3], 0, "secondAirline should't have votes");
   });
 
-  it('airline has no fund cannot add an Airline', async () => {
+  it('an airline with no fund cannot add an Airline', async () => {
     await truffleAssert.reverts(config.flightSuretyApp.addAirline(config.thirdAirline, 'Third Airline', {from: config.secondAirline}));
     let airline = await config.flightSuretyApp.getAirline.call(config.thirdAirline);
 
-    assert.equal(airline[0], '', 'thirdAirline should not be added by no fund airline');
+    assert.equal(airline[0], '', 'Airline should not be added by airline without funds');
   });
 
   it('firstAirline can vote to get second Airline registered', async () => {
@@ -109,13 +108,30 @@ contract('Flight Surety Tests', async (accounts) => {
     assert.equal(airline[2], 0, "secondAirline should't have funds");
   });
 
+  it("user can buy flight insuree", async () => {
+    await truffleAssert.passes(config.flightSuretyApp.buy(config.fifthAirline, "FLY-1",
+       timestamp, {from: config.passengerOne, value: web3.utils.toWei("1", "ether")}),
+        'The buy method fails');
+
+  });
+
+  it("withdrawel of flight insuree possible", async () => {
+    let balanceBefore = await web3.eth.getBalance(config.passengerOne);
+    await truffleAssert.passes(config.flightSuretyApp.processFlightStatus(config.fifthAirline, "FLY-1", timestamp, 20));
+    await truffleAssert.passes(config.flightSuretyApp.withdraw({from: config.passengerOne}));
+    let balanceAfter = await web3.eth.getBalance(config.passengerOne);
+
+    assert.ok(balanceAfter > balanceBefore, "Balance should have increased!");
+  });
+
   it('secondAirline can fund to secondAirline', async () => {
-    await config.flightSuretyApp.fund({from: config.secondAirline, value: intialFund});
+    let funding =  web3.utils.toWei("10", "ether");
+    await config.flightSuretyApp.fund({from: config.secondAirline, value: funding});
     let airline = await config.flightSuretyApp.getAirline.call(config.secondAirline);
 
     assert.equal(airline[0], 'Second Airline', 'Wrong name of second airline');
     assert.equal(airline[1], true, 'secondAirline is not registered');
-    assert.equal(airline[2], initialFund, "secondAirline should have funds");
+    assert.equal(airline[2], funding, "secondAirline should have funds");
   });
 
   it('secondAirline can add thirdAirline', async () => {
@@ -127,7 +143,7 @@ contract('Flight Surety Tests', async (accounts) => {
     assert.equal(airline[2], 0, "thirdAirline should not have funds");
   });
 
-  it('secondAirline can vote to get thirdAirline registered', async () => {
+  it('one airline can vote to get thirdAirline registered', async () => {
     await truffleAssert.passes(config.flightSuretyApp.vote(config.thirdAirline, {from: config.secondAirline}));
     let airline = await config.flightSuretyApp.getAirline.call(config.thirdAirline);
 
@@ -146,75 +162,13 @@ contract('Flight Surety Tests', async (accounts) => {
     assert.equal(airline[2], funding, "thirdAirline should have funds");
   });
 
-  it('thirdAirline can add fourthAirline', async () => {
-    await config.flightSuretyApp.addAirline(config.fourthAirline, 'Fourth Airline', {from: config.thirdAirline});
-    let airline = await config.flightSuretyApp.getAirline.call(config.fourthAirline);
-
-    assert.equal(airline[0], 'Fourth Airline', 'Wrong name of fourthAirline');
-    assert.equal(airline[1], false, 'fourthAirline should not be registered');
-    assert.equal(airline[2], 0, "fourthAirline should not have funds");
-  });
-
-  it('thirdAirline can vote to get fourthAirline registered', async () => {
-    await truffleAssert.passes(config.flightSuretyApp.vote(config.fourthAirline, {from: config.thirdAirline}));
-    let airline = await config.flightSuretyApp.getAirline.call(config.fourthAirline);
-
-    assert.equal(airline[1], true, 'fourthAirline is not registered');
-    assert.equal(airline[2], 0, "fourthAirline should not have fund");
-    assert.equal(airline[3], 1, "fourthAirline should have 1 vote");
-  });
-
-  it('fourthAirline can fund to fourthAirline', async () => {
-    let funding = web3.utils.toWei("13", "ether")
-    await config.flightSuretyApp.fund({from: config.fourthAirline, value: funding});
-    let airline = await config.flightSuretyApp.getAirline.call(config.fourthAirline);
-
-    assert.equal(airline[0], 'Fourth Airline', 'Wrong name of fourth airline');
-    assert.equal(airline[1], true, 'fourthAirline is not registered');
-    assert.equal(airline[2], funding, "fourthAirline should have funds");
-  });
-
-  it('fourthAirline can add fifthAirline', async () => {
-    await config.flightSuretyApp.addAirline(config.fifthAirline, 'Fifth Airline', {from: config.fourthAirline});
-    let airline = await config.flightSuretyApp.getAirline.call(config.fifthAirline);
-
-    assert.equal(airline[0], 'Fifth Airline', 'Wrong name of fifthAirline');
-    assert.equal(airline[1], false, 'fifthAirline should not be registered');
-    assert.equal(airline[2], 0, "fifthAirline should not have funds");
-  });
-
-  it('fourthAirline can not single vote to get fifthAirline registered', async () => {
-    await truffleAssert.passes(config.flightSuretyApp.vote(config.fifthAirline, {from: config.fourthAirline}));
-    let airline = await config.flightSuretyApp.getAirline.call(config.fifthAirline);
-
-    assert.equal(airline[1], false, 'fifthAirline should not be registered');
-  });
-
-  it('2 votes in the original 4 Airlines can get fifthAirline registered', async () => {
+  it('2 votes out of 4 Airlines can get fifthAirline registered', async () => {
     await truffleAssert.passes(config.flightSuretyApp.vote(config.fifthAirline, {from: config.thirdAirline}));
+    await truffleAssert.passes(config.flightSuretyApp.vote(config.fifthAirline, {from: config.firstAirline}));
     let airline = await config.flightSuretyApp.getAirline.call(config.fifthAirline);
 
     assert.equal(airline[1], true, 'fifthAirline should be registered by 2 votes');
     assert.equal(airline[2], 0, "fifthAirline should not have fund");
     assert.equal(airline[3], 2, "fifthAirline should have 2 vote");
   });
-
-  it("user can buy flight insuree", async () => {
-    await truffleAssert.passes(config.flightSuretyApp.buy(config.fifthAirline, lateFlight,
-       timestamp, {from: config.passengerOne, value: web3.utils.toWei("1", "ether")}),
-        'The but method fails');
-
-  });
-
-  it("passenger can withdraw flight insuree", async () => {
-    let balanceBefore = await web3.eth.getBalance(config.passengerOne);
-    await truffleAssert.passes(config.flightSuretyApp.processFlightStatus(config.fifthAirline, lateFlight, timestamp, 20));
-    await truffleAssert.passes(config.flightSuretyApp.withdraw({from: config.passengerOne}));
-
-    let balanceAfter = await web3.eth.getBalance(config.passengerOne);
-    let diff = balanceAfter - balanceBefore;
-    let compense = web3.utils.toWei("1", "ether") * 1.5;
-    assert.ok(diff > compense, "Balance Wrong!");
-  });
-
 });
